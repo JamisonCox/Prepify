f692f24613497bb40e9985eab242aef7d791dc83
jcox3 <jcox3@ncsu.edu>
2022-10-22 13:53:39 -0400

    Added Springboot

---
+++ src/main/java/com/hackduke/prepify/PrepifyApplication.java
@@ -0,0 +1,13 @@
+package com.hackduke.prepify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrepifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackduke/prepify/PrepifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackduke.prepify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrepifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
